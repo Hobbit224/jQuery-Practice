026bf736461dba4668b30e80ee06519548582bf4
Ian Booton <ianbooton@Ians-MacBook-Pro.local>
2017-05-15 11:17:10 -0400

    second commit

--- scripts.js
+++ scripts.js
@@ -31,6 +31,33 @@ $(document).ready(function(){
 		else if(whatToDo == 'toggle'){
 			$('#thing').toggle()
 		}
+		else if(whatToDo == 'html'){
+			$('#thing').html('I am new innerHTML.')
+		}
+		else if(whatToDo == 'text'){
+			$('#thing').text('I am new text.')
+		}
+		else if(whatToDo == 'prepend'){
+			$('#thing').prepend('I am new text.')
+		}
+		else if(whatToDo == 'append'){
+			$('#thing').append('I am new text.')
+		}
+		else if(whatToDo == 'css'){
+			$('#thing').css({
+				'color': 'orange',
+				'font-size': '40px',
+				'border-radius': '50%',
+				'background-color':'blue'
+			})
+		}
+		else if(whatToDo == 'addClass'){
+			$('#thing').addClass('crazy-css')
+		}
+		else if(whatToDo == 'removeClass'){
+			$('#thing').removeClass('crazy-css')
+		}
+
 
 	});
 
